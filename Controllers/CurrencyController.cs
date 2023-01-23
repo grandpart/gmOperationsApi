c534de0de8c55fbf4c0b2884ec8d14eb8ef7205b
superhugenious <eugenec@grandmark.co.za>
2023-01-23 22:21:49 +0200

    added currency

---
+++ Controllers/CurrencyController.cs
@@ -0,0 +1,159 @@
+﻿using Grandmark;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Zephry;
+
+namespace gmOperationsApi.Controllers
+{
+    
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        #region Get Specific - GET
+
+        [Route("currency/{curKey}")]
+        [HttpGet]
+        public string Load(int curKey, [FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(Request);
+
+            try
+            {
+                var vCurrency = new Currency
+                {
+                    EntKey = vLogonToken.Entity,
+                    CurKey = curKey
+                };
+                UserBridge.Invoke(CurrencyBusiness.Load, vCurrency, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, vCurrency.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+
+        #endregion
+
+        #region Read List - GET
+
+        [Route("currency")]
+        [HttpGet]
+        public string LoadList([FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(Request);
+            try
+            {
+                var vCurrencyList = new List<Currency>();
+                UserBridge.Invoke(CurrencyBusiness.LoadList, vCurrencyList, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, vCurrencyList.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+
+        #endregion
+
+        #region Create - POST
+        [Route("currency")]
+        [HttpPost]
+        public string Create([FromBody] Currency aCurrency, [FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(Request);
+            aCurrency.EntKey = vLogonToken.Entity;
+            try
+            {
+                UserBridge.Invoke(CurrencyBusiness.Insert, aCurrency, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, aCurrency.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+        #endregion
+
+        #region Update - PUT
+        [Route("currency/{curKey}")]
+        [HttpPut]
+        public string Update(int curKey, [FromBody] Currency aCurrency, [FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(Request);
+            aCurrency.EntKey = vLogonToken.Entity;
+            aCurrency.CurKey = curKey;
+            try
+            {
+                UserBridge.Invoke(CurrencyBusiness.Update, aCurrency, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, aCurrency.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+        #endregion
+
+        #region Delete - DELETE
+        [Route("currency/{curKey}")]
+        [HttpDelete]
+        public string Delete(int curKey, [FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(Request);
+            Currency vCurrencyKey = new();
+            vCurrencyKey.EntKey = vLogonToken.Entity;
+            vCurrencyKey.CurKey = curKey;
+            try
+            {
+                UserBridge.Invoke(CurrencyBusiness.Delete, vCurrencyKey, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, vCurrencyKey.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+        #endregion
+    }
+}
