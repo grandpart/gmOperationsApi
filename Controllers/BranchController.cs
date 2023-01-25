4ac3ffd38881029d030bd267955a48f595df8268
superhugenious <eugenec@grandmark.co.za>
2023-01-25 15:04:47 +0200

    added Branch and Warehouse

---
+++ Controllers/BranchController.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Zephry;
+
+namespace Grandmark
+{
+   
+    [ApiController]
+    public class BranchController : ControllerBase
+    {
+        #region Get Specific - GET
+
+        [Route("branch/{brhKey}")]
+        [HttpGet]
+        public string Load(int brhKey, [FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(HttpContext);
+
+            try
+            {
+                var vBranch = new Branch
+                {
+                    EntKey = vLogonToken.Entity,
+                    BrhKey = brhKey
+                };
+                UserBridge.Invoke(BranchBusiness.Load, vBranch, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, vBranch.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+
+        #endregion
+
+        #region Read List - GET
+
+        [Route("branch")]
+        [HttpGet]
+        public string LoadList([FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(HttpContext);
+            try
+            {
+
+                var vBranchList = new BranchCollection();
+                UserBridge.Invoke(BranchBusiness.LoadList, vBranchList, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, vBranchList.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+
+        #endregion
+
+        #region Create - POST
+        [Route("branch")]
+        [HttpPost]
+        public string Create([FromBody] Branch aBranch, [FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(HttpContext);
+            aBranch.EntKey = vLogonToken.Entity;
+            try
+            {
+                UserBridge.Invoke(BranchBusiness.Insert, aBranch, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, aBranch.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+        #endregion
+
+        #region Update - PUT
+        [Route("branch/{brhKey}")]
+        [HttpPut]
+        public string Update(int brhKey, [FromBody] Branch aBranch, [FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(HttpContext);
+            aBranch.EntKey = vLogonToken.Entity;
+            aBranch.BrhKey = brhKey;
+            try
+            {
+                UserBridge.Invoke(BranchBusiness.Update, aBranch, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, aBranch.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+        #endregion
+
+        #region Delete - DELETE
+        [Route("branch/{brhKey}")]
+        [HttpDelete]
+        public string Delete(int brhKey, [FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(HttpContext);
+            Branch vBranchKey = new();
+            vBranchKey.EntKey = vLogonToken.Entity;
+            vBranchKey.BrhKey = brhKey;
+            try
+            {
+                UserBridge.Invoke(BranchBusiness.Delete, vBranchKey, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, vBranchKey.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Controllers/WarehouseController.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Zephry;
+
+namespace Grandmark
+{
+    
+    [ApiController]
+    public class WarehouseController : ControllerBase
+    {
+        #region Get Specific - GET
+
+        [Route("warehouse/{whsKey}")]
+        [HttpGet]
+        public string Load(int whsKey, [FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(HttpContext);
+
+            try
+            {
+                var vWarehouse = new Warehouse
+                {
+                    EntKey = vLogonToken.Entity,
+                    WhsKey = whsKey
+                };
+                UserBridge.Invoke(WarehouseBusiness.Load, vWarehouse, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, vWarehouse.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+
+        #endregion
+
+        #region Read List - GET
+
+        [Route("warehouse")]
+        [HttpGet]
+        public string LoadList([FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(HttpContext);
+            try
+            {
+                
+                var vWarehouseList = new WarehouseCollection();
+                UserBridge.Invoke(WarehouseBusiness.LoadList, vWarehouseList, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, vWarehouseList.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+
+        #endregion
+
+        #region Create - POST
+        [Route("warehouse")]
+        [HttpPost]
+        public string Create([FromBody] Warehouse aWarehouse, [FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(HttpContext);
+            aWarehouse.EntKey = vLogonToken.Entity;
+            try
+            {
+                UserBridge.Invoke(WarehouseBusiness.Insert, aWarehouse, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, aWarehouse.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+        #endregion
+
+        #region Update - PUT
+        [Route("warehouse/{whsKey}")]
+        [HttpPut]
+        public string Update(int whsKey, [FromBody] Warehouse aWarehouse, [FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(HttpContext);
+            aWarehouse.EntKey = vLogonToken.Entity;
+            aWarehouse.WhsKey = whsKey;
+            try
+            {
+                UserBridge.Invoke(WarehouseBusiness.Update, aWarehouse, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, aWarehouse.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+        #endregion
+
+        #region Delete - DELETE
+        [Route("warehouse/{whsKey}")]
+        [HttpDelete]
+        public string Delete(int whsKey, [FromServices] Connection aConnection)
+        {
+            var vLogonToken = Utils.GetLogonToken(HttpContext);
+            Warehouse vWarehouseKey = new();
+            vWarehouseKey.EntKey = vLogonToken.Entity;
+            vWarehouseKey.WhsKey = whsKey;
+            try
+            {
+                UserBridge.Invoke(WarehouseBusiness.Delete, vWarehouseKey, vLogonToken, aConnection);
+                Response.StatusCode = StatusCodes.Status200OK;
+                // NB, change this to a pure success message, no return
+                return Utils.StatusJson(null, vWarehouseKey.SerializeToJson());
+            }
+            catch (TransactionStatusException tx)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Utils.StatusJson(new TransactionStatus(tx.TransactionResult, tx.Message), string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Utils.StatusJson(new TransactionStatus(TransactionResult.General, ex.Message), string.Empty);
+            }
+        }
+        #endregion
+    }
+}
